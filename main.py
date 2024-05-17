fdce821ceb6667574cc60b49fe31a27bbbffbeff
GamezConZ <drgamezcarlos@gmail.com>
2024-05-17 17:16:04 -0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+# This a simple portofolio project to showcase python skills under the Codecademy lessons
+# It's going to be a game questionaire for friends, with a minimum of two players
+# One or more friends write a personal questions quiz and the others have to guess the answer
+# (Based on losing the apartment scene on Friends)
+# TODO List:
+# Running the program to specify if the user is going to create or answer the questions.
+# Input the questions creator's data and funcionality (multiple choice - boolean)
+# Create the questions based on what the user specified
+# Save the questions to a json file
+# Running the program as an answerer
+# Input the answerer's data and functionality
+# loading the json file
+# Asking the question
+# Returning the results
